b493d78b0d6cac08de5c477f419a4e06e55a7990
Wouter de Bruijn <wouter@hedium.nl>
2022-01-05 11:46:24 +0100

    :sparkles: Added status codes to errors

--- errors.ts
+++ errors.ts
@@ -1,26 +1,28 @@
 export class FilemakerError extends Error {
   public response;
+  public statusCode;
 
-  constructor(message: string, response: Record<never, never>) {
+  constructor(message: string, response: Record<never, never>, statusCode: number) {
     super(message);
     this.response = response;
+    this.statusCode = statusCode;
   }
 }
 
 export class LoginError extends FilemakerError {
   constructor(response: Record<never, never>) {
-    super("Invalid username or password!", response);
+    super("Invalid username or password!", response, 401);
   }
 }
 
 export class StatusCodeError extends FilemakerError {
   constructor(response: Record<never, never>) {
-    super("Unexpected filemaker statuscode", response);
+    super("Unexpected filemaker statuscode", response, 502);
   }
 }
 
 export class MissingRecord extends FilemakerError {
   constructor(response: Record<never, never>) {
-    super("No matching records", response);
+    super("No matching records", response, 404);
   }
 }
